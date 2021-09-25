186003118aaf72bce6254b750a90c6e361f14352
hawkup <spiritsquare@gmail.com>
2021-09-25 16:50:18 +0700

    Init project

---
+++ src/main/java/com/example/myspringbootplayground/MySpringBootPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.example.myspringbootplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBootPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myspringbootplayground/MySpringBootPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myspringbootplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringBootPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
